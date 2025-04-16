22e40c6f65f23c789792dd0348b125f8558c33a3
Jesus Blanco <jesus.blancof96@gmail.com>
2025-04-16 12:41:00 -0400

    refactor: update heading and description in HomePage component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,15 +6,12 @@ export const HomePage: FC = () => {
     <div>
       <StarsBackground />
       <div className="max-w-2xl lg:max-w-4xl mx-auto absolute top-1/2 left-1/2  z-10 -translate-y-1/2 -translate-x-1/2">
-        <h1 className="text-gray-900 dark:text-gray-100 drop-shadow-lg dark:drop-shadow-white/40 drop-shadow-black/40 m-0 p-0 font-bold md:text-7xl 2xl:text-9xl">
-          Hello there!
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-700 to-neutral-400 dark:from-neutral-200 dark:to-neutral-400 text-center font-sans font-bold drop-shadow-md dark:drop-shadow-white/40 drop-shadow-black/40">
+          I'm Jesus Blanco
         </h1>
-        <h6>
-          <span className="text-gray-700 dark:text-gray-200 text-xl 2xl:text-3xl">
-            I'm Jesus Blanco, a senior Front-end developer with over 6 years of experience, passionate about architect
-            clean, maintainable, and performant web applications that deliver exceptional user experiences.
-          </span>
-          <br />
+        <h6 className="dark:text-neutral-400 max-w-lg my-2 text-md text-center relative z-10">
+          A senior Front-end developer with over 6 years of experience, passionate about architect clean, maintainable,
+          and performant web applications that deliver exceptional user experiences.
         </h6>
       </div>
     </div>
