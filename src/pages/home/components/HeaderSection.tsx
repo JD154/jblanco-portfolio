ffd99097d0e34a5741fc98981dc1e05cd9c17063
Jesus Blanco <jesus.blancof96@gmail.com>
2025-04-22 11:14:58 -0400

    refactor: adjust padding in NavigationBar and update HeaderSection layout for consistency

--- src/pages/home/components/HeaderSection.tsx
+++ src/pages/home/components/HeaderSection.tsx
@@ -4,9 +4,9 @@ export const HeaderSection = () => {
   return (
     <section
       id="header-section"
-      className="max-w-2xl lg:max-w-4xl mx-auto absolute top-1/2 left-1/2  z-10 -translate-y-1/2 -translate-x-1/2 flex items-center flex-col justify-center text-center"
+      className="w-full px-4 lg:px-0 lg:max-w-4xl mx-auto absolute top-1/2 left-1/2  z-10 -translate-y-1/2 -translate-x-1/2 flex items-center flex-col justify-center text-center"
     >
-      <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-500 dark:from-neutral-200 dark:to-neutral-400 text-center font-sans font-bold drop-shadow-md dark:drop-shadow-white/40 drop-shadow-black/40">
+      <h1 className="relative z-10 text-5xl lg:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-500 dark:from-neutral-200 dark:to-neutral-400 text-center font-sans font-bold drop-shadow-md dark:drop-shadow-white/40 drop-shadow-black/40">
         I'm Jesus Blanco
       </h1>
       <h6 className="dark:text-neutral-400 max-w-lg mt-4 mb-8 text-md text-center relative z-10">
