014f2ce4541632c92342cd6b3a16f88699389328
Jesus Blanco <jesus.blancof96@gmail.com>
2025-05-27 15:55:13 -0400

    feat: refactor ProjectsSection to replace AnimatedParagraph with static text for project description

--- src/pages/home/components/ProjectsSection/index.tsx
+++ src/pages/home/components/ProjectsSection/index.tsx
@@ -1,10 +1,8 @@
-import { FC } from 'react';
+import { AnimatedHeading } from '@/components/general/AnimatedHeading';
 import projects from '@/data/projects.json';
 import { useAnimation, useInView } from 'motion/react';
-import { useRef, useEffect } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import { ProjectCard } from './components/ProjectCard';
-import { AnimatedHeading } from '@/components/general/AnimatedHeading';
-import { AnimatedParagraph } from '@/components/general/AnimatedParagraph';
 
 // ProjectsSection component
 export const ProjectsSection: FC = () => {
@@ -23,21 +21,16 @@ export const ProjectsSection: FC = () => {
     <section className="py-20 px-4 max-w-6xl mx-auto relative z-10" ref={ref}>
       <div className="mb-12 text-center">
         <AnimatedHeading text={'Projects'} fontSize="4rem" sensitivity={0.02} />
-        <AnimatedParagraph
-          lines={[
-            'Here is a curated list of the projects I have worked on',
-            'showcasing my ability to create clean, functional, and maintainable user interfaces.',
-          ]}
-          visibleLines={2}
-          className="dark:text-neutral-300 text-md relative z-10 max-w-[750px] mx-auto"
-        />
+        <p className="dark:text-neutral-300 text-md relative z-10 max-w-[750px] mx-auto">
+          Here is a curated list of the projects I have worked on, showcasing my ability to create clean, functional,
+          and maintainable user interfaces.
+        </p>
       </div>
 
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {projects.map((project, idx) => (
           <ProjectCard
             key={project.title}
-            {...project}
             delay={isInView ? 0.15 * idx : 0}
             animate={controls}
             initial="hidden"
@@ -45,6 +38,7 @@ export const ProjectsSection: FC = () => {
               hidden: { opacity: 0, y: 32 },
               visible: { opacity: 1, y: 0 },
             }}
+            {...project}
           />
         ))}
       </ul>
