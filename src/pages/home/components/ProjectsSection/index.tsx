eac3a59f31a5efa8f103ccce009a4f59b63b1476
Jesus Blanco <jesus.blancof96@gmail.com>
2025-05-27 12:08:36 -0400

    feat: add AnimatedParagraph to ProjectsSection for enhanced project description

--- src/pages/home/components/ProjectsSection/index.tsx
+++ src/pages/home/components/ProjectsSection/index.tsx
@@ -4,6 +4,7 @@ import { useAnimation, useInView } from 'motion/react';
 import { useRef, useEffect } from 'react';
 import { ProjectCard } from './components/ProjectCard';
 import { AnimatedHeading } from '@/components/general/AnimatedHeading';
+import { AnimatedParagraph } from '@/components/general/AnimatedParagraph';
 
 // ProjectsSection component
 export const ProjectsSection: FC = () => {
@@ -20,7 +21,18 @@ export const ProjectsSection: FC = () => {
 
   return (
     <section className="py-20 px-4 max-w-6xl mx-auto relative z-10" ref={ref}>
-      <AnimatedHeading text={'Projects'} fontSize="4rem" sensitivity={0.03} className="mb-12" />
+      <div className="mb-12 text-center">
+        <AnimatedHeading text={'Projects'} fontSize="4rem" sensitivity={0.02} />
+        <AnimatedParagraph
+          lines={[
+            'Here is a curated list of the projects I have worked on',
+            'showcasing my ability to create clean, functional, and maintainable user interfaces.',
+          ]}
+          visibleLines={2}
+          className="dark:text-neutral-300 text-md relative z-10 max-w-[750px] mx-auto"
+        />
+      </div>
+
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {projects.map((project, idx) => (
           <ProjectCard
